1597f59db83b5c1f71c0607daf52eafce3a058f7
Mohit Sharma <mohit13@outlook.com>
2022-05-09 10:30:05 +0200

    .

--- main.tf
+++ main.tf
@@ -1,27 +1,60 @@
-# Configure the Azure provider
 terraform {
+  required_version = ">=1.1.3"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = ">=2.93.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.1.0"
     }
   }
 
-  required_version = ">= 1.1.0"
+  backend "azurerm" {
+    resource_group_name  = "terraformRG"
+    storage_account_name = "terrastate13"
+    container_name       = "remotestate"
+    subscription_id      = "058f5639-3033-4b94-8a57-a06abd997a9d"
+    key                  = "tfstateremote.tfstate"
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+provider "local" {
+  # Configuration options
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+  subscription_id = var.subscription_id
+  #skip_provider_registration = true
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.rgname
-  location = "westus2"
+module "basic" {
+  source      = "./modules/basic"
+  location    = var.location
+  tags        = var.tags
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.nwname
-  address_space       = ["10.0.0.0/16"]
-  location            = "westus2"
-  resource_group_name = azurerm_resource_group.rg.name
+module "function" {
+  source      = "./modules/function"
+  location    = var.location
+}
+
+module "local" {
+  source      = "./modules/local"
+  location    = var.location
+}
+
+module "variables" {
+  source      = "./modules/variable"
+  location    = var.location
 }
